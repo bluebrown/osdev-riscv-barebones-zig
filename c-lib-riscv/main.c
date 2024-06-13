7ca5464cd88c4032b727e135ce2753351499c169
bluebrown <rainbowstack@gmail.com>
2024-06-13 22:30:29 +0200

    feat: vectored trap
    Signed-off-by: bluebrown <rainbowstack@gmail.com>

--- c-lib-riscv/main.c
+++ c-lib-riscv/main.c
@@ -8,19 +8,23 @@
   while (1) {                                                                  \
   }
 
-void irqHandler();
+// trap vec is a 4 byte aligned
+// jump switch in trapvec.S
+extern void trapvec();
 
 int main() {
-  // load the trap as first thing, to catch any exception.
-  // the setup uart in order to print debug messages.
-  csrw(mtvec, &irqHandler);
+  // load the trap first, so any exception during init
+  // will be reported
+  csrw(mtvec, (uint32_t)trapvec | 1);
 
+  // mmio driver instances
   struct UartDriver u = UartDriver(UART_BASE);
   struct Writer w = (struct Writer){&u, (Write *)uart_rtxWrite};
 
+  // configure uart before printing
   uart_fifoInit(&u);
 
-  // start the real initialization
+  // start the initialization
   fprint(&w, "init\n");
 
   // enable interrupts
@@ -50,38 +54,40 @@ int main() {
     wfi;
 }
 
-void irqHandler() {
+void trap0() {
   char buf[35];
   struct UartDriver u = {(uint8_t *)UART_BASE};
   struct Writer w = (struct Writer){&u, (Write *)uart_rtxWrite};
 
-  fprint(&w, "irq: ");
+  fprint(&w, "irq: base: ");
 
   struct XCause cause = MCause();
   fprint(&w, itoa(2, cause.is_interrupt, buf));
   fprint(&w, ": code: ");
   fprint(&w, itoa(16, cause.code, buf));
+  fprint(&w, ": ");
 
-  if (!cause.is_interrupt) {
-    fprint(&w, ": exception: ");
+  if (cause.is_interrupt)
+    fprint(&w, irq_names[cause.code]);
+  else
     fprint(&w, exception_names[cause.code]);
-    hotloop;
-  }
 
-  fprint(&w, ": interrupt: ");
-  fprint(&w, irq_names[cause.code]);
+  fprint(&w, "\n");
+  hotloop;
+}
 
-  if (cause.code != IRQ_MACHINE_EXTERNAL_INTERRUPT) {
-    fprint(&w, "\n");
-    hotloop;
-  }
+void trapExternal() {
+  char buf[35];
+  struct UartDriver u = {(uint8_t *)UART_BASE};
+  struct Writer w = (struct Writer){&u, (Write *)uart_rtxWrite};
+
+  fprint(&w, "irq: external: ");
 
   struct PlicDriver p = PlicDriver(PLIC_BASE);
-
   int idx = plic_wordIndex(0);
   size_t src = plic_claim(&p, idx);
 
-  fprint(&w, ": PLIC source: ");
+  fprint(&w, "PLIC source: ");
   fprint(&w, itoa(16, src, buf));
   fprint(&w, "\n");
 
